6acef9e1e477ab44ac6d6efd10e22aafedd05d0b
Corey <corey@razorgames.org>
2016-10-08 03:42:36 -0400

    first commit

---
+++ src/qt/test/uritests.h
@@ -0,0 +1,15 @@
+#ifndef URIINVTS_H
+#define URIINVTS_H
+
+#include <QTest>
+#include <QObject>
+
+class URITests : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void uriTests();
+};
+
+#endif // URIINVTS_H
